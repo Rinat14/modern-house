7c21b4510cdbce60c734c03c1d87d68ab17ebcee
Rinat Samandarov <rinatsamandarov14@gmail.com>
2023-01-28 20:46:39 +0500

    Set color and polette are finished

--- js/main.js
+++ js/main.js
@@ -1,18 +1,55 @@
 'use strict';
+'bg-[#000]';
 
-const toolbarBtns = document.querySelectorAll('.toolbar-btns');
+const toolbarBtns = document.querySelectorAll('.toolbar-btns'),
+	colorNavigation = document.querySelectorAll('.color-navigation'),
+	colorInput = document.querySelector('.my-color'),
+	settingColor = document.querySelector('.set-color'),
+	polette = document.querySelector('.polette');
+const submitBtn = document.querySelector('.submit');
 
 toolbarBtns.forEach(btn => {
 	btn.addEventListener('click', testToggle);
 });
 
+colorNavigation.forEach(btn => {
+	btn.addEventListener('click', (event) => {
+		if (event.target.classList.contains('first')) {
+			settingColor.classList.add('flex');
+			settingColor.classList.remove('hidden');
+			polette.classList.remove('flex');
+			polette.classList.add('hidden');
+		} else if (event.target.classList.contains('second')) {
+			polette.classList.remove('hidden');
+			polette.classList.add('flex');
+			settingColor.classList.remove('flex');
+			settingColor.classList.add('hidden');
+		} else {return null}
+		removingWithForeach(colorNavigation, 'active');
+		event.target.classList.add('active');
+	});
+});
+colorInput.addEventListener('change', () => {
+	const card = document.querySelector('.setting-color');
+	const value = colorInput.value;
+	card.classList.add(`bg-[${value}]`);
+});
+
+submitBtn.addEventListener('click', () => {
+	colorInput.value = '#000';
+})
+
 function testToggle(event) {
-	if (event.target.classList.contains('toolbar-btns__active')) {
-		event.target.classList.remove('toolbar-btns__active');
+	if (event.target.classList.contains('active')) {
+		event.target.classList.remove('active');
 	} else {
-		toolbarBtns.forEach(el => {
-			el.classList.remove('toolbar-btns__active');
-			event.target.classList.add('toolbar-btns__active');
-		});
+		removingWithForeach(toolbarBtns, 'active');
+		event.target.classList.add('active');
 	}
+}
+
+function removingWithForeach(array, className) {
+	array.forEach(el => {
+		el.classList.remove(className);
+	})
 }
